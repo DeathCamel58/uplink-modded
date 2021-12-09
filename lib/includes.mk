d374d56e597fab86ab8d883eb50bdd3b0b7eaf36
Dylan Corrales <deathcamel58@gmail.com>
2021-12-09 14:15:58 -0500

    Update `lib` makefile

--- lib/includes.mk
+++ lib/includes.mk
@@ -6,9 +6,12 @@ INCLUDES += $(LIBS:%=-I../%)
 INCLUDES += -I$(INSTALLDIR)/include 
 #INCLUDES += -I$(INSTALLDIR)/include/gltt
 
-INCLUDES += -I$(CONTRIBDIR)/install/include/freetype2
-INCLUDES += -I$(CONTRIBDIR)/install/include/freetype2/freetype
+INCLUDES += -I$(CONTRIBDIR)/freetype/include
+#INCLUDES += -I$(CONTRIBDIR)/install/include/freetype2/freetype
+INCLUDES += -I$(CONTRIBDIR)/FTGL-2.1.2/include
 #INCLUDES += -I$(CONTRIBDIR)/ftgl_dist/src
+INCLUDES += -I$(CONTRIBDIR)/SDL_mixer/include
+INCLUDES += -I$(CONTRIBDIR)/SDL/include
 INCLUDES += -I$(CONTRIBDIR)
 CXXFLAGS += $(INCLUDES)
 CXXFLAGS += -g
