663d2a767ae6f73ed64628533b101b78857374da
Dylan Corrales <deathcamel58@gmail.com>
2022-06-20 20:02:16 -0400

    Documentation: News

--- uplink/src/world/company/news.h
+++ uplink/src/world/company/news.h
@@ -1,14 +1,4 @@
 
-/*
-
-  News object
-
-	Represents a single news story, posted onto 
-	one of the news servers
-
-  */
-
-
 #ifndef included_news_h
 #define included_news_h
 
@@ -31,21 +21,45 @@
 
 // ============================================================================
 
-
+//! News object
+/**
+ * Represents a single news story, posted onto one of the news servers
+ */
 class News : public UplinkObject
 {
 
 protected:
 
+    /**
+     * The text content of the news item
+     */
 	string details;
 
 public:
 
+    /**
+     * The date of the news item
+     */
 	Date date;
+
+	/**
+	 * The headline of the news item
+	 */
 	string headline;
-	
+
+	/**
+	 * The type of the news item
+	 */
 	int NEWSTYPE;
+
+	/**
+	 * Varies based on newNEWSTYPE
+	 */
 	char data1 [SIZE_NEWS_DATA];
+
+	/**
+	 * Varies based on newNEWSTYPE
+	 */
 	char data2 [SIZE_NEWS_DATA];
 
 public:
@@ -53,12 +67,37 @@ public:
 	News ();
 	~News () override;
 
+	/**
+	 * Sets the date of the news item
+	 * @param newdate The new date
+	 */
 	void SetDate     ( Date *newdate );
+
+	/**
+	 * Sets the headline of the news item
+	 * @param newheadline The news headline
+	 */
 	void SetHeadline (const string &newheadline );
+
+	/**
+	 * Sets the text content of the news item
+	 * @param newdetails The news content
+	 */
 	void SetDetails  (const string &newdetails );
 
+	/**
+	 * Gets the text content of the news item
+	 * @return
+	 */
 	string GetDetails ();
 
+	/**
+	 * Sets the data of the news item
+	 * @param newNEWSTYPE The type of the news item
+	 * @param newdata1 Varies based on newNEWSTYPE
+	 * @param newdata2 Varies based on newNEWSTYPE
+	 * @note Depending on the newNEWSTYPE, newdata1 and newdata2 can represent different data
+	 */
 	void SetData (int newNEWSTYPE, const string &newdata1 = "", const string &newdata2 = "" );
 
 	// Common functions
